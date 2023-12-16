ad6c6b2e850cd7da3c60dcfee1c7b707a0ae0428
alvinswx <alvinswx@maxis.com.my>
2023-12-16 22:05:23 +0800

    adding subnet.tf file

---
+++ subnet.tf
@@ -0,0 +1,45 @@
+resource "aws_subnet public_1"{
+    vpc_id = aws_vpc.main.id
+    cidr_block = 10.10.0.0/26
+    availability_zone = "ap-southeast-1a"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "public-ap-southeast-1a"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb" = 1
+    }
+}
+
+resource "aws_subnet public_2"{
+    vpc_id = aws_vpc.main.id
+    cidr_block = 10.10.0.64/26
+    availability_zone = "ap-southeast-1b"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "public-ap-southeast-1b"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb" = 1
+    }
+}
+
+resource "aws_subnet private_1"{
+    vpc_id = aws_vpc.main.id
+    cidr_block = 10.10.0.128/26
+    availability_zone = "ap-southeast-1a"
+    tags = {
+        Name = "private-ap-southeast-1a"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/internal-elb" = 1
+    }
+}
+
+resource "aws_subnet private_2"{
+    vpc_id = aws_vpc.main.id
+    cidr_block = 10.10.0.192/26
+    availability_zone = "ap-southeast-1b"
+    tags = {
+        Name = "private-ap-southeast-1b"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/internal-elb" = 1
+    }
+}
