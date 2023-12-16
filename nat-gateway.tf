c40a4adbb4cd34d913ff6b247183e68735bd42c7
alvinswx <alvinswx@maxis.com.my>
2023-12-16 23:33:18 +0800

    modified nat-gateway to use subnet public_1b instead of subnet public_2

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "NAT1a"{
     allocation_id = aws_eip.nat_gw_1.id
-    subnet_id = aws_subnet.public_1.id
+    subnet_id = aws_subnet.public_1a.id
     tags = {
         Name = "Nat Gateway 1a"
     }
@@ -8,7 +8,7 @@ resource "aws_nat_gateway" "NAT1a"{
 
 resource "aws_nat_gateway" "NAT1b"{
     allocation_id = aws_eip.nat_gw_2.id
-    subnet_id = aws_subnet.public_2.id
+    subnet_id = aws_subnet.public_1b.id
     tags = {
         Name = "Nat Gateway 1b"
     }
