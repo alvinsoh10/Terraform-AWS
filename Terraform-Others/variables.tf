851eb7a3df431f1f53160eba259c826ed0c169f0
alvin <alvinsoh10@hotmail.com>
2024-03-20 19:05:47 +0800

    added variables.tf

--- Terraform-Others/variables.tf
+++ Terraform-Others/variables.tf
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
 variable availability_zone_1a {
   type        = string
   default     = "ap-southeast-1c"
   description = "description"
 }
+=======
+variable "region" {
+    type = string
+    default = "ap-southeast-1"
+}
+
+variable "availability_zone_1a" {
+    type = string
+    default = "ap-southeast-1a"
+}
+>>>>>>> dc4d9ab (added variables.tf)
