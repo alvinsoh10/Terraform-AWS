79ba6607bf7905f9161c214ae947607ed4d003f9
alvin <alvinsoh10@hotmail.com>
2025-07-16 14:21:46 +0800

    added route 53

--- Base-Infra/rds.tf
+++ Base-Infra/rds.tf
@@ -4,9 +4,11 @@ resource "aws_db_instance" "default" {
   engine               = "postgres"
   engine_version       = "17.4"
   instance_class       = "db.t4g.micro"
+  identifier           = "alvin-test"
   username             = "postgres"
   password             = "password"
   parameter_group_name = "default.postgres17"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
 }
+
---
+++ Base-Infra/route53.tf
@@ -0,0 +1,15 @@
+resource "aws_route53_zone" "private" {
+  name = "alvin.com"
+
+  vpc {
+    vpc_id = aws_vpc.main.id
+  }
+}
+
+resource "aws_route53_record" "postgresdb" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "db.alvin.com"
+  type    = "CNAME"
+  ttl     = "30"
+  records = [aws_db_instance.default.endpoint]
+}
