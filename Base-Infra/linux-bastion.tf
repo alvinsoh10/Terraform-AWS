8c81dc248631023eee7a29cfac7c62052ada1a31
alvin <alvinsoh10@hotmail.com>
2025-07-17 19:56:59 +0800

    added bastion

---
+++ Base-Infra/linux-bastion.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.private_1a.id
+
+  tags = {
+    Name = "linux-bastion"
+  }
+}
